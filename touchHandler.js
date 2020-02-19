56f0f116a1d35413a8994cdb5effd87207110ee6
Vyrrhus <baptistedesoubrie@gmail.com>
2020-02-19 13:55:07 +0100

    Update touchHandler.js

--- touchHandler.js
+++ touchHandler.js
@@ -4,26 +4,6 @@ const CONTEXT	= CANVAS.getContext('2d');
 const PI2		= Math.PI * 2;
 var WIDTH = document.getElementById('body').offsetWidth;
 var HEIGHT = document.getElementById('body').offsetHeight;
-//
-//class TouchHandler {
-//	// CONSTRUCTOR
-//	constructor() {
-//		this.cache = new Array();
-//	}
-//	
-//	// GETTERS
-//	
-//	// METHODS
-//	add(touch) {
-//		for (let i = 0 ; i < this.cache.length ; i++) {
-//			if (this.cache[i].identifier === touch.identifier)
-//		}
-//	}
-//	
-//	// STATIC METHODS
-//}
-
-
 
 document.ontouchstart	= ontouchstart_handler;
 document.ontouchend		= ontouchend_handler;
@@ -35,28 +15,16 @@ function ontouchstart_handler(ev) {
 	// NB DE TOUCHES
 	// IDENTIFIANTS
 	// CANVAS DRAW ELLIPSE DE TOUCHE
-	for (let i = 0 ; i < ev.touches.length ; i++) {
-		showInfo('start', ev.touches[i], '.touches');
-	}
 	for (let i = 0 ; i < ev.changedTouches.length ; i++) {
 		showInfo('start', ev.changedTouches[i], '.changedTouches');
 		drawTouch(ev.changedTouches[i], 'rgba(0,0,255,0.8)')
 	}
-	for (let i = 0 ; i < ev.targetTouches.length ; i++) {
-		showInfo('start', ev.targetTouches[i], '.targetTouches');
-	}
 }
 function ontouchend_handler(ev) {
 	// LOG END
-	for (let i = 0 ; i < ev.touches.length ; i++) {
-		showInfo('end', ev.touches[i], '.touches');
-	}
 	for (let i = 0 ; i < ev.changedTouches.length ; i++) {
 		showInfo('end', ev.changedTouches[i], '.changedTouches');
 		drawTouch(ev.changedTouches[i], 'rgba(255,0,0,0.8)')
-	}
-	for (let i = 0 ; i < ev.targetTouches.length ; i++) {
-		showInfo('end', ev.targetTouches[i], '.targetTouches');
 	}
 }
 function ontouchmove_handler(ev) {
@@ -64,28 +32,16 @@ function ontouchmove_handler(ev) {
 	// NB DE TOUCHES
 	// IDENTIFIANTS
 	// SI DEUX TOUCHES: DISTANCE ENTRE EUX EN PIXELS, COEFFICIENT DIRECTEUR
-	for (let i = 0 ; i < ev.touches.length ; i++) {
-		showInfo('move', ev.touches[i], '.touches');
-	}
 	for (let i = 0 ; i < ev.changedTouches.length ; i++) {
 		showInfo('move', ev.changedTouches[i], '.changedTouches');
 		drawTouch(ev.changedTouches[i], 'rgba(0,255,0,0.1)')
 	}
-	for (let i = 0 ; i < ev.targetTouches.length ; i++) {
-		showInfo('move', ev.targetTouches[i], '.targetTouches');
-	}
 }
 function ontouchcancel_handler(ev) {
 	// LOG CANCEL
-	for (let i = 0 ; i < ev.touches.length ; i++) {
-		showInfo('cancel', ev.touches[i], '.touches');
-	}
 	for (let i = 0 ; i < ev.changedTouches.length ; i++) {
 		showInfo('cancel', ev.changedTouches[i], '.changedTouches');
-		drawTouch(ev.changedTouches[i], 'rgba(255,255,0,0.3)')
-	}
-	for (let i = 0 ; i < ev.targetTouches.length ; i++) {
-		showInfo('cancel', ev.targetTouches[i], '.targetTouches');
+		drawTouch(ev.changedTouches[i], 'rgba(255,255,0,0.8)')
 	}
 }
 
@@ -108,8 +64,7 @@ function showInfo(handler, touch, type) {
 	pDOM.setAttribute('class', type);
 	touchDOM.appendChild(pDOM);
 
-	pDOM.innerHTML = 	'id: ' + touch.identifier + ' - ' + type + '<br>' +
-						'screen: ' + touch.screenX + ' ; ' + touch.screenY + '<br>' +
+	pDOM.innerHTML = 	'id: ' + touch.identifier + ' - ' + '<br>' +
 						'client: ' + touch.clientX + ' ; ' + touch.clientY + '<br>' +
 						'radius: ' + touch.radiusX + ' ; ' + touch.radiusY + ' - rotation: ' + touch.rotationAngle + ' - force: ' + touch.force;
 }
@@ -142,3 +97,6 @@ window.onload = function() {
 	resize();
 	window.addEventListener('resize', function() {resize();}, false);
 }
+
+// ON CONSERVE UNIQUEMENT .changedTouches (pas .touches ni .targetTouches) car seul élément conservé par chaque event.
+// ON CONSERVE UNIQUEMENT .clientX .clientY.
